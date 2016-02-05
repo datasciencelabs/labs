2f77ca9e9fbe4d7e2efbdd63fe409c3cae186e4e
ririzarr <rairizarry@gmail.com>
2016-02-05 00:20:36 -0500

    type edit

--- insights-on-poverty/get-oecd.R
+++ insights-on-poverty/get-oecd.R
@@ -1,4 +1,9 @@
 library(XML)
 html_tab = readHTMLTable('http://www.oecd.org/about/membersandpartners/list-oecd-member-countries.htm')
-oecd_countries <- tolower(levels(html_tab[[2]][[2]]))
+tmp <- tolower(levels(html_tab[[2]][[2]]))[-1]
+tmp <- strsplit(oecd_countries," ")
+oecd_countries <- sapply(tmp, function(x){
+  paste(Hmisc::capitalize(x), collapse=" ")
+})
 saveRDS(oecd_countries,file=file.path("data","oecd-countries.rds"))
+
